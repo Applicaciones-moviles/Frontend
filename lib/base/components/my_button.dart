403019b6590143e43be656f2a368ae150238c0fc
Scott-Huachaca <89089754+Scott-Huachaca@users.noreply.github.com>
2024-09-26 14:43:30 -0500

    feat: add comment

--- lib/base/components/my_button.dart
+++ lib/base/components/my_button.dart
@@ -26,6 +26,7 @@ class MyButton extends StatelessWidget {
             color: Colors.white,
             fontWeight: FontWeight.w600,
             fontFamily: 'Inter',
+            //ejemplo
           ),
         ),
       ),
